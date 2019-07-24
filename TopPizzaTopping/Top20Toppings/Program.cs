795724092a2d2b163f56a28f3be5ebe1fd4b4411
harsh <harshs1695@gmail.com>
2019-07-24 23:21:07 +0530

    Topping (Deserialize Data and Show result)

---
+++ TopPizzaTopping/Top20Toppings/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace Top20Toppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            var data = httpClient.GetAsync("http://files.olo.com/pizzas.json").Result.Content.ReadAsStringAsync().Result;
+
+            var dataDeserialize = JsonConvert.DeserializeObject<List<ToppingDto>>(data);
+
+            var toppingList = dataDeserialize.Select(x => string.Join(",", x.Toppings));
+
+            Dictionary<string, int> toppings = new Dictionary<string, int>();
+
+            foreach (var item in toppingList)
+            {
+                if (toppings.ContainsKey(item))
+                {
+                    toppings[item] += 1;
+                }
+                else
+                {
+                    toppings.Add(item, 1);
+                }
+            }
+
+            var top20Topping = toppings.OrderByDescending(x => x.Value).Take(20);
+
+            int rank = 1;
+
+            Console.WriteLine("Rank" + "        " + "Count" + "         " + "Topping");
+
+            foreach (var item in top20Topping)
+            {
+                Console.WriteLine("  " + rank++ + "         " + item.Value + "          " + item.Key);
+
+                Console.WriteLine("===============================================");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TopPizzaTopping/Top20Toppings/ToppingDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Top20Toppings
+{
+    public class ToppingDto
+    {
+        public IEnumerable<string> Toppings = new List<string>();
+    }
+}
